9b7a7260814f92d44ccb7e9fabdd9e451562325c
Matheus Amaral <mramaral@poli.ufrj.br>
2019-02-13 14:15:49 -0200

    Java 1 Aula 4

---
+++ alura/sintaxe-variaveis-e-fluxo/src/TestaPontoFlutuante.java
@@ -0,0 +1,14 @@
+
+public class TestaPontoFlutuante {
+	public static void main(String[] args) {
+		
+		double salario = 1250.70;
+		
+		System.out.println("o meu salario é: " + salario);
+		
+		double divisao = 5.0/2;
+		System.out.println("Resultado resolvendo o truncamento da divisao: " + divisao);
+		
+	}
+
+}
