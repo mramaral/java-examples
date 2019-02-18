ccf5387d306f19ab635ec8d237ae26587839e3a8
Matheus Amaral <mramaral@poli.ufrj.br>
2019-02-18 18:22:21 -0300

    Java parte 6, aula 2

--- alura/bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteArrayReferencias.java
+++ alura/bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteArrayReferencias.java
@@ -5,15 +5,21 @@ import br.com.bytebank.banco.modelo.*;
 public class TesteArrayReferencias {
 
 	public static void main(String[] args) {
-		ContaCorrente[] contas = new ContaCorrente[5];
+		Conta[] contas = new Conta[5];
 		
 		ContaCorrente cc1 = new ContaCorrente(22, 11);
 		ContaCorrente cc2 = new ContaCorrente(22, 22);
+		ContaPoupanca cp1 = new ContaPoupanca(22, 12);
 		
 		contas[0] = cc1;
 		contas[1] = cc2;
+		contas[2] = cp1;
 		
 		System.out.println(contas[0].getNumero());
+		
+		ContaCorrente ref = (ContaCorrente)contas[0];
+		System.out.println(cc2.getNumero());
+		System.out.println(ref.getNumero());
 
 	}
 
